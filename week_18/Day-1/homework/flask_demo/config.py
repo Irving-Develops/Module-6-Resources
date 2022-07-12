47ba78968385f2cbdfba5da0a7aea519fb0956a8
Irving Arreola Palacios <irving.a789@gmail.com>
2022-07-12 10:01:56 -0500

    homework

---
+++ week_18/Day-1/homework/flask_demo/config.py
@@ -0,0 +1,6 @@
+import os
+
+
+class Config(object):
+    GREETING = 'Salutations, superior students!'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-for-devs'
--- week_18/Day-1/homework/flask_demo/simple.py
+++ week_18/Day-1/homework/flask_demo/simple.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import (Flask, render_template)
+from config import Config
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def index():
+    # return f'<h1>{app.config["GREETING"]}</h1>'
+    return render_template('index.html', sitename='My Sample')
+
+
